80e96bb40154a20435f4f5d2f4b5c28bf7b8ce23
snowy <trigger.snowii@gmail.com>
2025-02-24 19:00:32 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// 줄바꿈
-document.getElementById('inputText').addEventListener('input', function() {
-    const inputText = this.value;
-    const lines = inputText.split('\n');
-    const numberedLines = lines.map((line, index) => `${index + 1}: ${line}`);
-    document.getElementById('outputText').textContent = numberedLines.join('\n');
-});
